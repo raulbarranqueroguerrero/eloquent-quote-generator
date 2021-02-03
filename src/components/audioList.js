cdeff1c5d4334029c3a31c42c4810cb166e28d69
raulbarranqueroguerrero <barranqueroguerrero@gmail.com>
2021-02-04 00:31:51 +0100

    Resize icon image to impove performance

--- src/components/audioList.js
+++ src/components/audioList.js
@@ -2,13 +2,9 @@ const audioList = [{
       name: 'Bird and Diz',
       singer: 'Charlie Parker',
       cover:
-      'https://firebasestorage.googleapis.com/v0/b/host-medias-b0b6d.appspot.com/o/Charle-Parker-Resized.png?alt=media&token=55c13e9f-7cf2-4337-8253-974aaf7718ba',
+      'https://firebasestorage.googleapis.com/v0/b/host-medias-b0b6d.appspot.com/o/Charlie-180.png?alt=media&token=786c8962-e3d1-4c6b-88ff-5b3def113c58',
       musicSrc:
       'https://firebasestorage.googleapis.com/v0/b/host-medias-b0b6d.appspot.com/o/Charlie%20Parker%20-%20Bird%20and%20Diz%20(1952).mp3?alt=media&token=a4c5f6e3-9122-40e3-8b4d-438943e8bc56'
-      // support async fetch music src. eg.
-      // musicSrc: async () => {
-      //   return await fetch('/api')
-      // },
 }]
 
 export default audioList
