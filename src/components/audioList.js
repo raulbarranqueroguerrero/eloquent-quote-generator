6093eec54dd02a24dbb69bc3359ac1f6dbb67810
raulbarranqueroguerrero <barranqueroguerrero@gmail.com>
2021-02-04 00:36:23 +0100

    Adjust icon image to 140px

--- src/components/audioList.js
+++ src/components/audioList.js
@@ -2,7 +2,7 @@ const audioList = [{
       name: 'Bird and Diz',
       singer: 'Charlie Parker',
       cover:
-      'https://firebasestorage.googleapis.com/v0/b/host-medias-b0b6d.appspot.com/o/Charlie-60.png?alt=media&token=52174ebd-9d9c-4352-964d-227fb4d01e03',
+      'https://firebasestorage.googleapis.com/v0/b/host-medias-b0b6d.appspot.com/o/Charlie-140.png?alt=media&token=5a49159f-c1f1-46ec-87aa-0f337c600eea',
       musicSrc:
       'https://firebasestorage.googleapis.com/v0/b/host-medias-b0b6d.appspot.com/o/Charlie%20Parker%20-%20Bird%20and%20Diz%20(1952).mp3?alt=media&token=a4c5f6e3-9122-40e3-8b4d-438943e8bc56'
 }]
