6c391a6bb7491b8b694f5cd7d8f7675e35a30bd5
raulbarranqueroguerrero <barranqueroguerrero@gmail.com>
2021-02-04 14:19:19 +0100

    Add songs to playlist

--- src/components/MusicPlayer.js
+++ src/components/MusicPlayer.js
@@ -10,6 +10,7 @@ function MusicPlayer() {
             defaultPosition={{top: 8, left: 8}}
             style={{fontFamily: 'sans-serif', fontWeight: 'bolder'}}
             autoplay={true}
+            glassBg={true}
         />
     )
 }
--- src/components/audioList.js
+++ src/components/audioList.js
@@ -1,10 +1,28 @@
-const audioList = [{
+const audioList = [
+   {
+      name: 'Funny Valentine',
+      singer: 'Chet Buker',
+      cover:
+      'https://firebasestorage.googleapis.com/v0/b/host-medias-b0b6d.appspot.com/o/Chet-140png.png?alt=media&token=f5638ee0-36ac-4e10-8844-0f9af88634cc',
+      musicSrc:
+      'https://firebasestorage.googleapis.com/v0/b/host-medias-b0b6d.appspot.com/o/yt1s.com%20-%20Chet%20Baker%20%20My%20Funny%20Valentine.mp3?alt=media&token=1e0b6fbf-6342-4b88-91d0-51a04db0510c'
+   },
+   {
+      name: 'Freddie Freeloader',
+      singer: 'Miles Davis',
+      cover:
+      'https://firebasestorage.googleapis.com/v0/b/host-medias-b0b6d.appspot.com/o/Miles-ALbum-150.png?alt=media&token=75c275b9-a9a8-486b-94bc-066c83cd0bf0',
+      musicSrc:
+      'https://firebasestorage.googleapis.com/v0/b/host-medias-b0b6d.appspot.com/o/yt1s.com%20-%20Miles%20Davis%20%20Freddie%20Freeloader%20Audio.mp3?alt=media&token=48832f4d-3acc-4992-947a-a41561f73886'
+   },
+   { 
       name: 'Bird and Diz',
       singer: 'Charlie Parker',
       cover:
       'https://firebasestorage.googleapis.com/v0/b/host-medias-b0b6d.appspot.com/o/Charlie-140.png?alt=media&token=5a49159f-c1f1-46ec-87aa-0f337c600eea',
       musicSrc:
       'https://firebasestorage.googleapis.com/v0/b/host-medias-b0b6d.appspot.com/o/Charlie%20Parker%20-%20Bird%20and%20Diz%20(1952).mp3?alt=media&token=a4c5f6e3-9122-40e3-8b4d-438943e8bc56'
-}]
+   },
+]
 
 export default audioList
